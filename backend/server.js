6808a141aebcfdfe7ccf2d1cc5c55f23ac3225ed
shabanalsmdysy-crypto <shabanalsmdysy@gmail.com>
2026-07-25 21:34:51 +0300

    style(backend): update app.listen console log message format

--- backend/server.js
+++ backend/server.js
@@ -33,8 +33,8 @@ try {
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 let prisma;
